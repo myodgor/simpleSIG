29531a8c4113562e21054ed5fcbcdc45d64e69d2
myodgor <73735209+myodgor@users.noreply.github.com>
2022-12-02 10:04:00 +0300

    Update simpleSIG.py

--- simpleSIG.py
+++ simpleSIG.py
@@ -1,20 +1,20 @@
 import PySimpleGUI as sg
-import os
+from sys import exit
+from os import path, getcwd, chdir
 import subprocess
-import sys
 
-#Проверка наличия ПО Крипто ПРО
-if os.path.exists(r'C:\Program Files\Crypto Pro\CSP\csptest.exe'):
+# Проверка наличия ПО Крипто ПРО
+if path.exists(r'C:\Program Files\Crypto Pro\CSP\csptest.exe'):
     cspp = r'C:\Program Files\Crypto Pro\CSP\csptest.exe'
     certp = 'C:\Program Files\Crypto Pro\CSP'
-elif os.path.exists(r'C:\Program Files (x86)\Crypto Pro\CSP\csptest.exe'):
+elif path.exists(r'C:\Program Files (x86)\Crypto Pro\CSP\csptest.exe'):
     cspp = r'C:\Program Files (x86)\Crypto Pro\CSP\csptest.exe'
     certp = 'C:\Program Files (x86)\Crypto Pro\CSP'
 else:
     sg.Popup('Ошибка!\nПрограммное обеспечение Крипто ПРО не найдено.')
-    sys.exit()
+    exit()
 
-#Запуск программы
+# Запуск программы
 layout = [
     [sg.Text('Вставьте ключ с ЭЦП')],
     [sg.Submit('Запустить'), sg.Cancel('Отмена')]
@@ -25,72 +25,96 @@ while True:
     event, values = window.read()
     if event == 'Отмена' or event == sg.WIN_CLOSED:
         window.close()
-        sys.exit()
+        exit()
     else:
         break
 window.close()
 
-#Загрузка установленных сертификатов
-cwd = os.getcwd()
+# Загрузка установленных сертификатов
+cwd = getcwd()
 try:
-    os.chdir(certp)
+    chdir(certp)
     result = subprocess.run(['certmgr.exe', '-list'], shell=True, capture_output=True, text=True, encoding='866')
 except:
     sg.Popup('Произошла ошибка запуска ПО Крипто ПРО.')
-    sys.exit()
-os.chdir(cwd)
+    exit()
+chdir(cwd)
 
-#Парсинг возвращенный данных
+# Парсинг возвращенный данных
 text = result.stdout
-while "  " in text:    
+while "  " in text:
     text = text.replace("  ", " ")
 while '""' in text:
     text = text.replace('""', '"')
 if 'Subject : ' in text:
-    sname = 'Subject : ' #На английском
+    sname = 'Subject : '
 elif 'Субъект : ' in text:
-    sname = 'Субъект : ' #На русском
+    sname = 'Субъект : '
 else:
     sg.Popup('Ошибка!\nВ системе не обнаружены ЭЦП.')
-    sys.exit()
+    exit()
 start = 0
 g = []
 org = []
 s = 0
 while start < (len(text)):
-    cn1 = text.find('Subject : ', start)
+    cn1 = text.find(sname, start)
     if cn1 == -1:
         break
     cn2 = text.find('\n', cn1)
-    g.append(text[cn1+10:cn2])
+
+    organ = text[cn1 + 10:cn2]
     start = cn2
+    cn1 = text.find('SHA1 Hash', start)
+    if cn1 == -1:
+        break
+    cn2 = text.find('\n', cn1)
+    organ += ', KeyID=' + text[cn1 + 12:cn2]
+    start = cn2
+    cn1 = text.find('Not valid after', start)
+    if cn1 == -1:
+        break
+    cn2 = text.find('\n', cn1)
+    organ += ', NVA=' + text[cn1 + 18:cn1 + 28]
+    start = cn2
+    g.append(organ)
 for i in g:
     ii = i.split(', ')
-    name = ['', '', '']
+    name = ['', '', '', '', '']
     for l in ii:
+        if l[0:3] == "OID":
+            oidinn = l.split('=')
+            name[1] = str(int(oidinn[1]))
         if l[0:3] == "CN=":
             name[0] = l[3:].lstrip('"')
-        if l[0:2] == "E=":
-            name[2] = l[2:]
-        if l[0:4] == "ИНН=":
+        if l[0:6] == "KeyID=":
+            name[3] = l[6:]
+        if l[0:4] == "NVA=":
+            name[4] = l[4:]
+        if l[0:2] == "G=":
+            name[2] = l[2:] + ')'
+        if l[0:3] == "SN=":
+            name[2] = '(' + l[3:] + ' ' + name[2]
+        if l[0:4] == "ИНН=" and name[1] == '':
             name[1] = l[4:].lstrip("0")
-    if name[1] != '' and name[2] != '':
-        org.append(name[0]+'; '+name[2]+'; '+name[1]) 
+    if name[0] != '' and name[1] != '':
+        org.append(name[0] + '; ' + name[4] + '; ' + name[2] + '; ' + name[1] + '; ' + name[3])
 if len(org) == 0:
-    sg.Popup('Ошибка!\nВ системе не обнаружены ЭЦП.')
-    sys.exit()
+    sg.Popup('Ошибка!\nНе удалось обработать список ЭЦП.')
+    exit()
 
-#Обработка запроса
+
+# Обработка запроса
 def ronga():
     ddd = values['kto'][0].split("; ")
-    filename = values[0] #.replace("/", "\\")[:-1]
+    filename = values[0]  # .replace("/", "\\")[:-1]
     if values['tosig'] == True:
-        pusk = cspp + ' -sfsign -sign -add -in "' + filename + '" -out "' + os.path.splitext(filename)[0] + '_sig' + os.path.splitext(filename)[1] + '" -my ' + ddd[1]
+        pusk = cspp + ' -sfsign -sign -add -in "' + filename + '" -out "' + path.splitext(filename)[0] + '_sig' + \
+               path.splitext(filename)[1] + '" -my ' + ddd[1]
     else:
-        pusk = cspp + ' -sfsign -sign -detached -add -in "' + filename + '" -out "' + filename + '.sig" -my ' + ddd[1]
-    inin = ddd[2]
-    if inin != "":
-        pusk = pusk + ',' + inin
+        pusk = cspp + ' -sfsign -sign -detached -add -in "' + filename + '" -out "' + filename + '.sig" -my ' + ddd[4]
+    if values['sigtime'] == True:
+        pusk = pusk + ' -addsigtime'
     par_ol = values['password']
     if par_ol != "":
         pusk = pusk + ' -password ' + par_ol
@@ -103,18 +127,21 @@ def ronga():
         sg.Popup('Прозошла ошибка, попробуйте еще раз!')
     print(data[0])
     sg.Popup('Файл подписан!')
+    return
 
-#Создание окна программы
+
+# Создание окна программы
 layout = [
     [sg.Text('Выберите владельца ЭЦП')],
     [sg.Listbox(values=org, size=(88, len(org)), key='kto', enable_events=True)],
     [sg.Text('Введите ПИН закрытого ключа*'), sg.InputText(key='password', password_char='*', size=(15, 1))],
     [sg.Text('* Необязательный пункт. Запрашивается через ПО Крипто ПРО.')],
-    [sg.Text('Выберите файл'), sg.InputText(), sg.FileBrowse('Выбрать', key = 'file')],
+    [sg.Text('Выберите файл'), sg.InputText(), sg.FileBrowse('Выбрать', key='file')],
     [sg.Checkbox('Присоединенная подпись', key='tosig')],
+    [sg.Checkbox('Со штампом времени', key='sigtime')],
     [sg.Button('Подписать'), sg.Button('Очистить форму'), sg.Button('Выход')],
     [sg.Output(key='vyxod', size=(88, 20))],
-    [sg.Text('simpleSIG v1.1 (2020.11.16)'), sg.Button('О программе')]
+    [sg.Text('simpleSIG v1.2 (2022.12.02)'), sg.Button('О программе')]
 ]
 window = sg.Window('Подписать файлы ЭЦП', layout)
 
@@ -123,7 +150,7 @@ while True:
     event, values = window.read()
     if event in (None, 'Выход', sg.WIN_CLOSED):
         window.close()
-        sys.exit()
+        exit()
     if event == 'Очистить форму':
         window['password']('')
         window['file']('Выбрать')
@@ -131,7 +158,8 @@ while True:
         window[0]('')
         window['tosig'](False)
     if event == 'О программе':
-        sg.Popup('simpleSIG - подписывать файлы ЭЦП легко!\nАвтор: Максим Ёдгоров\ngithub: https://github.com/myodgor/simpleSIG')
+        sg.Popup(
+            'simpleSIG - подписывать файлы ЭЦП легко!\nАвтор: Максим Ёдгоров\ngithub: https://github.com/myodgor/simpleSIG')
     if event == 'Подписать':
         if len(values['kto']) == 0:
             sg.Popup('Ошибка! Выберите ЭЦП.')
@@ -139,4 +167,3 @@ while True:
             sg.Popup('Ошибка! Выберите файл.')
         else:
             ronga()
-
